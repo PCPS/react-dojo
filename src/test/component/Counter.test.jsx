2903cc09cf46eb33f54f0fe0d88bc5f5affd0399
KaisHaddadin <kais.haddadin@endress.com>
2024-01-12 13:18:58 +0100

    remove stuff

--- src/test/component/Counter.test.jsx
+++ src/test/component/Counter.test.jsx
@@ -1,39 +1,9 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Counter from '../../component/Counter';
 
 describe('<Counter/>', () => {
-  it('renders the needed button when loading', () => {
-    render(<Counter />);
-    expect(screen.getByRole('button', { name: /Increase/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /Decrease/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /Reset/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /set Count/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /Save Count/i })).toBeInTheDocument();
-    expect(screen.getByText('You clicked 0 times')).toBeTruthy();
-  });
-
-  it('adapt counter when action buttons are clicked', () => {
+  xit('renders the needed button when loading', () => {
     render(<Counter />);
     expect(screen.getByText('You clicked 0 times')).toBeTruthy();
-    fireEvent.click(screen.getByRole('button', { name: /Increase/i }));
-    fireEvent.click(screen.getByRole('button', { name: /Increase/i }));
-    fireEvent.click(screen.getByRole('button', { name: /Increase/i }));
-    fireEvent.click(screen.getByRole('button', { name: /Increase/i }));
-    expect(screen.getByText('You clicked 4 times')).toBeTruthy();
-    fireEvent.click(screen.getByRole('button', { name: /Decrease/i }));
-    fireEvent.click(screen.getByRole('button', { name: /Decrease/i }));
-    expect(screen.getByText('You clicked 2 times')).toBeTruthy();
-    fireEvent.click(screen.getByRole('button', { name: /Reset/i }));
-    expect(screen.getByText('You clicked 0 times')).toBeTruthy();
-  });
-
-  it('show saved counter in list when action buttons are clicked', () => {
-    render(<Counter />);
-    expect(screen.getByText('You clicked 0 times')).toBeTruthy();
-    fireEvent.click(screen.getByRole('button', { name: /Increase/i }));
-    fireEvent.click(screen.getByRole('button', { name: /Increase/i }));
-    expect(screen.getByText('You clicked 2 times')).toBeTruthy();
-    fireEvent.click(screen.getByRole('button', { name: /Save Count/i }));
-    expect(screen.getByText('Counter 1: 2')).toBeTruthy();
   });
 });
