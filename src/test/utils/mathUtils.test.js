8e48bdd06886937f062c129afe04c360ed5e185a
Valentino Rusconi <valentino.rusconi@endress.com>
2024-01-17 16:18:35 +0100

    add: dependencies for smooth dojo
    jest + proptypes

---
+++ src/test/utils/mathUtils.test.js
@@ -0,0 +1,6 @@
+const sum = require('../../utils/mathUtils');
+
+// example unit test
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ src/utils/mathUtils.js
@@ -0,0 +1,6 @@
+// example of a function that we want to test
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
